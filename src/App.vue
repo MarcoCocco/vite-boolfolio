b21c6c2d475e22b1be804c2c384d9c54222c9647
MarcoCocco <marcococco28@gmail.com>
2023-05-29 16:31:50 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container pt-4">
+    <h1>Tutti i progetti</h1>
+  </div>
+</template>
+
+<style scoped></style>
