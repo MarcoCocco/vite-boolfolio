aa0317fe183533627e2d5cf2aa35488a02786b5c
MarcoCocco <marcococco28@gmail.com>
2023-05-29 23:29:27 +0200

    feat: gestita l'impaginazione dei progetti

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,6 +8,10 @@ export default {
     data() {
         return {
             projects: [],
+
+            currentPage: 1,
+            lastPage: 0,
+
         }
     },
 
@@ -17,10 +21,27 @@ export default {
 
     methods: {
         getProjects() {
-            axios.get("http://127.0.0.1:8000/api/projects").then(res => {
-                this.projects = res.data.results;
+            axios.get('http://127.0.0.1:8000/api/projects?page=' + this.currentPage).then(res => {
+                console.log(res.data.results.data)
+                this.projects = res.data.results.data;
+                this.lastPage = res.data.results.last_page;
+
             });
-        }
+        },
+
+        nextPage() {
+            if (this.currentPage < this.lastPage) {
+                this.currentPage++;
+                this.getProjects();
+            }
+        },
+
+        prevPage() {
+            if (this.currentPage > 1) {
+                this.currentPage--;
+                this.getProjects();
+            }
+        },
     },
 
     components: { AppProjectCard }
@@ -28,15 +49,47 @@ export default {
 </script>
 
 <template>
-    <div class="container pt-4 text-center">
-        <h1 class="pb-4">Tutti i progetti</h1>
+    <main class="container">
+        <h1 class="text-center p-4">Tutti i progetti</h1>
         <hr>
-        <div class="row">
-            <div v-for="project in projects" class="col-6 mb-5">
-                <AppProjectCard :project="project"></AppProjectCard>
+        <div v-if="projects.length > 0" class="projects pt-2 text-center">
+            <div class="row">
+                <div v-for="project in projects" class="col-6 mb-2">
+                    <AppProjectCard :project="project"></AppProjectCard>
+                </div>
             </div>
         </div>
-    </div>
+        <div v-else class="loading-screen">
+            <div class="spinner-grow text-primary" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+        <div class="page-buttons d-flex justify-content-center gap-4">
+            <button @click="prevPage()" class="btn btn-primary">Prev</button>
+            <button @click="nextPage()" class="btn btn-primary">Next</button>
+        </div>
+    </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.loading-screen {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    min-height: 670px;
+}
+
+.projects {
+    overflow-x: hidden;
+    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    width: 100%;
+}
+
+.btn {
+    text-align: center;
+}
+</style>
