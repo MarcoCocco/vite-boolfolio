3dc1344cf0b46d189aca4bc5ef8a7aef28cae32e
MarcoCocco <marcococco28@gmail.com>
2023-05-29 17:39:21 +0200

    feat: creato il componente AppProjectCard con la relativa lista viasualizzata in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <script>
 import axios from 'axios';
+import AppProjectCard from './components/AppProjectCard.vue';
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       projects: [],
-    }
+    };
   },
-
   created() {
     this.getProjects();
   },
-
   methods: {
     getProjects() {
-      axios.get('http://127.0.0.1:8000/api/projects').then(res => {
+      axios.get("http://127.0.0.1:8000/api/projects").then(res => {
         this.projects = res.data.results;
-      })
+      });
     }
-  }
+  },
+  components: { AppProjectCard }
 }
 </script>
 
@@ -26,21 +26,11 @@ export default {
   <div class="container pt-4 text-center">
     <h1 class="pb-4">Tutti i progetti</h1>
     <hr>
-<table class="table table-striped">
-  <thead>
-    <th>Titolo</th>
-    <th>Slug</th>
-    <th>Descrizione</th>
-  </thead>
-
-  <tbody>
-    <tr v-for="project in projects">
-      <td>{{ project.title }}</td>
-      <td>{{ project.slug }}</td>
-      <td>{{ project.description }}</td>
-    </tr>
-  </tbody>
-</table>
+    <div class="row">
+      <div v-for="project in projects" class="col-6 mb-5">
+        <AppProjectCard :project="project"></AppProjectCard>
+      </div>
+    </div>
   </div>
 </template>
 
---
+++ src/components/AppProjectCard.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+    name: 'AppProjectCard',
+    data() {
+        return {
+            baseUrl: 'http://127.0.0.1:8000/',
+        }
+    },
+
+    props: {
+        project: Object,
+    },
+
+    computed: {
+        shortDescription() {
+            if (this.project.description.length > 100) {
+                return this.project.description.substring(0, 100) + '...';
+            } else {
+                return this.project.description;
+            }
+        },
+
+        projectImage() {
+            if (this.project.project_image === null) {
+                return 'https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg';
+            } else {
+                return this.baseUrl + 'storage/' + this.project.project_image;
+            }
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="project card">
+            <img :src="projectImage" class="project-image card-img-top" alt="Immagine">
+            <div class="card-body">
+                <h5 class="card-title">{{ project.title }}</h5>
+                <p class="card-text">{{ shortDescription }}</p>
+                <a href="#" class="btn btn-primary">Dettagli del progetto</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.project.card {
+    .project-image {
+        height: 300px;
+        object-fit: cover;
+        object-position: center;
+    }
+}
+</style>
